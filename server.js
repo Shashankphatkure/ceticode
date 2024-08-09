c4b3717367f46e46acc97a613e105d7e2bbe8fa3
Shashank Phatkure <shashankphatkure@gmail.com>
2024-08-09 20:01:59 +0530

    updates

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const nodemailer = require("nodemailer");
 const { PDFDocument, rgb } = require("pdf-lib");
+const fontkit = require("@pdf-lib/fontkit");
 const fs = require("fs").promises;
 const path = require("path");
 
@@ -33,31 +34,71 @@ app.post("/generate-certificate", async (req, res) => {
     const templatePdfBytes = await fs.readFile(templatePath);
     const pdfDoc = await PDFDocument.load(templatePdfBytes);
 
+    // Register fontkit
+    pdfDoc.registerFontkit(fontkit);
+
+    // Embed the Montserrat-Bold font
+    const fontPath = "./Montserrat-Bold.ttf";
+    const fontBytes = await fs.readFile(fontPath);
+    const montserratBoldFont = await pdfDoc.embedFont(fontBytes);
+
     // Get the first page of the document
     const pages = pdfDoc.getPages();
     const firstPage = pages[0];
+    const { width, height } = firstPage.getSize();
 
-    // Add text to the PDF
-    firstPage.drawText(name, {
-      x: 300,
-      y: 350,
-      size: 30,
-      color: rgb(0, 0, 0),
-    });
+    // Convert #0f1176 to RGB values (normalized to 0-1 range)
+    const color = {
+      r: 15 / 255,
+      g: 17 / 255,
+      b: 118 / 255,
+    };
 
+    // Function to draw centered text
+    const drawCenteredText = (text, y, fontSize) => {
+      const textWidth = montserratBoldFont.widthOfTextAtSize(text, fontSize);
+      const x = (width - textWidth) / 2;
+      firstPage.drawText(text, {
+        x,
+        y,
+        size: fontSize,
+        font: montserratBoldFont,
+        color: rgb(color.r, color.g, color.b),
+      });
+    };
+
+    // firstPage.drawText(designation, {
+    //   x: 240,
+    //   y: 225,
+    //   size: 16,
+    //   font: montserratBoldFont,
+    //   color: rgb(color.r, color.g, color.b),
+    // });
+
+    // Right-align the designation
+    const designationWidth = montserratBoldFont.widthOfTextAtSize(
+      designation,
+      16
+    );
+    const designationX = width - designationWidth - 540; // 20 is right margin
     firstPage.drawText(designation, {
-      x: 300,
-      y: 300,
-      size: 20,
-      color: rgb(0, 0, 0),
+      x: designationX,
+      y: 225,
+      size: 16,
+      font: montserratBoldFont,
+      color: rgb(color.r, color.g, color.b),
     });
 
     firstPage.drawText(date, {
-      x: 300,
-      y: 250,
-      size: 20,
-      color: rgb(0, 0, 0),
+      x: 205,
+      y: 115,
+      size: 14,
+      font: montserratBoldFont,
+      color: rgb(color.r, color.g, color.b),
     });
+
+    // Add centered text to the PDF
+    drawCenteredText(name, 285, 30);
 
     // Save the PDF
     const pdfBytes = await pdfDoc.save();
@@ -79,7 +120,6 @@ app.post("/generate-certificate", async (req, res) => {
     };
 
     await transporter.sendMail(mailOptions);
-
     res.send("Certificate generated and sent successfully!");
   } catch (error) {
     console.error(error);
